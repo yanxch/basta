213730d137f366a6ebfdee10e1ee38e713ba6d8e
Christian Janker <janker.christ@gmail.com>
2019-07-22 19:55:30 +0200

    add jasmine

---
+++ jasmine-standalone-3.4.0/spec/Additon.spec.js
@@ -0,0 +1,13 @@
+/* tslint:disable */ 
+describe('Object under test', function () {
+
+  it('should do this', function() {
+    let result = add(1, 2);
+    expect(result).toBe(3);
+  });
+
+  it('should do that', function() {
+    let result = add(1, 11);
+    expect(result).toBe(12);
+  });
+});
